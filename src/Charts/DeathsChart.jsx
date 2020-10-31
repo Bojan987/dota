50f36a3e32f7ad3674c1438c70403d921ce99226
bojan987 <bojanmaj@hotmail.com>
2020-10-31 20:38:49 +0100

    overflow

--- src/Charts/DeathsChart.jsx
+++ src/Charts/DeathsChart.jsx
@@ -14,7 +14,7 @@ export const DeathsChart = ({playerDeaths})=>{
         labels: totalDeaths.slice(0,20),
         datasets: [
           {
-            label: 'Total number of games, ended with selected number of deaths',
+            label: `Total number of games ended with selected number of deaths`,
             backgroundColor: 'rgba(42, 169, 219,0.3)',
             borderColor: 'rgba(119, 249, 253, 0.8)',
             borderWidth: 1,
@@ -29,7 +29,9 @@ export const DeathsChart = ({playerDeaths})=>{
 
     return (
         <>
+        
         <div className='barChar'>
+     
             <Bar
           data={data}
           width={400}
